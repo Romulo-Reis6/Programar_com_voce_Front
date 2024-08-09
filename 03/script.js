22549fa81c47a218aadebb4aca4195473d9e09f1
Rômulo Reis <romuloreis06@gmail.com>
2024-08-08 22:21:43 -0300

    fix: Condicional da pesquisa

--- 03/script.js
+++ 03/script.js
@@ -5,11 +5,12 @@ inputSearch.addEventListener("input", () => {
   cards.forEach((card) => {
     const tittle = card.querySelector(".card-title").innerText.toLowerCase();
     const text = card.querySelector(".card-text").innerText.toLowerCase();
+    const textCard = tittle + " " + text;
     const textSearch = document
       .querySelector("#input-search")
       .value.toLowerCase();
 
-    tittle.includes(textSearch) | text.includes(textSearch)
+    textCard.includes(textSearch)
       ? card.parentElement.classList.remove("d-none")
       : card.parentElement.classList.add("d-none");
   });
